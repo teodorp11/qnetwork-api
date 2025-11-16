bb0a0b72f2cf649dac426a6a00daca4eb7d57e0f
teodorp11 <teodor.posea@gmail.com>
2025-11-16 20:43:06 +0200

    Commit 4: Updating Models Folder by adding IndustrialDeviceNetworkMapping Class

---
+++ qnetwork-api/Models/IndustrialDeviceNetworkMapping.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace qnetwork_api.Models
+{
+    public class IndustrialDeviceNetworkMapping
+    {
+        [Key]
+        public Guid ID { get; set; }
+        [Required]
+        
+        public Guid IndustrialDeviceID { get; set; }
+        
+        [ForeignKey(nameof(IndustrialDeviceID))]
+        public IndustrialDevice? IndustrialDevice { get; set; }
+
+        [Required]
+        public Guid NetworkID { get; set; }
+
+        [ForeignKey(nameof(NetworkID))]
+        public Network? Network { get; set; }
+
+        public string? Role { get; set; }
+    }
+}
